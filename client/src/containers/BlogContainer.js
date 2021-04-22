02ae50b557ab686ea77ab1cd7bb880443c5fd454
A7abhilash <a7abhilash@gmail.com>
2021-04-22 15:59:13 +0530

    added bookmarking feature on frontend

--- client/src/containers/BlogContainer.js
+++ client/src/containers/BlogContainer.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, FlatList } from "react-native";
 import { useAuth } from "../contexts/AuthContext";
 import { useMsg } from "../contexts/MsgContext";
-import { updateLikes } from "../db";
+import { updateLikes, updateBookmark, getLoggedInUserDetails } from "../db";
 import { globalStyles } from "../styles/globalStyles";
 import Card from "./Card";
 
 export default function BlogContainer({ displayBlogs, isProfile, navigation }) {
   const { user } = useAuth();
-  const { setToast } = useMsg();
+  const { setToast, setAlert } = useMsg();
+  const [savedLists, setSavedLists] = useState([]);
 
+  useEffect(() => {
+    getLoggedInUserDetails(user?._id).then((data) => {
+      setSavedLists(data.savedBlogsList.blogs);
+    });
+  }, [setSavedLists, user?._id]);
+
+  const addBookmark = async (blogId) => {
+    if (!savedLists.includes(blogId)) {
+      let updatedList = [blogId, ...savedLists];
+      let res = await updateBookmark(updatedList, user?._id);
+      setSavedLists(updatedList);
+      setToast(res.status === 200 ? "Bookmark added ★" : res.error);
+    } else setAlert("Wrong Action", "Blog is already saved", "Understood");
+  };
+
+  const removeBookmark = async (blogId) => {
+    if (savedLists.includes(blogId)) {
+      let updatedList = savedLists.filter(
+        (savedId) => savedId.toString() !== blogId.toString()
+      );
+      let res = await updateBookmark(updatedList, user?._id);
+      setSavedLists(updatedList);
+      setAlert(res.status === 200 ? "Bookmark removed ☆" : res.error);
+    } else alert("Wrong Action", "Blog wasn't saved", "Understood");
+  };
   const likeBlog = async (blog) => {
     if (!blog.likes.includes(user._id)) {
       let updatedLikes = {
@@ -44,6 +70,9 @@ export default function BlogContainer({ displayBlogs, isProfile, navigation }) {
           blog={item}
           access={user?._id === item.user._id}
           isProfile={false}
+          addBookmark={addBookmark}
+          isBookmarked={savedLists.includes(item._id)}
+          removeBookmark={removeBookmark}
           likes={item.likes}
           isLiked={item.likes.includes(user?._id)}
           likeBlog={likeBlog}
